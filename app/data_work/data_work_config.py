106661430429173d0416602433e9b5263093c015
Axeratos <nikitalukianitcares@gmail.com>
2022-09-26 14:07:29 +0300

    Users db initialized

--- app/data_work/data_work_config.py
+++ app/data_work/data_work_config.py
@@ -4,4 +4,4 @@ STORAGE_DIR = Path(__file__).parent.joinpath("storage")
 
 CSV_FILE = STORAGE_DIR.joinpath("people_data.csv")
 TXT_FILE = STORAGE_DIR.joinpath("txt_for_read.txt")
-USERS_DB = STORAGE_DIR.joinpath("users.db")
+USERS_DB_PATH = STORAGE_DIR.joinpath("users.db")
