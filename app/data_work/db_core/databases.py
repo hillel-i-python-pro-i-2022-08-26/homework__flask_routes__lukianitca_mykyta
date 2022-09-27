20de03940d5af72aee6954112902866d2484f828
Axeratos <nikitalukianitcares@gmail.com>
2022-09-27 11:51:52 +0300

    Method description renamed

--- app/data_work/db_core/databases.py
+++ app/data_work/db_core/databases.py
@@ -6,8 +6,8 @@ from app.data_work.data_work_config import USERS_DB_PATH
 
 class UsersDB(ABC):
     def __init__(self):
-        self.connection = sqlite3.connect(USERS_DB_PATH)
+        self.connection: sqlite3.Connection = sqlite3.connect(USERS_DB_PATH)
 
     @abstractmethod
     def create_table(self):
-        """Table creation"""
+        """Table create method"""
